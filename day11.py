191c9519acee53520e231960254d292941a58050
NKrvavica <nino.krvavica@gmail.com>
2021-12-11 11:14:49 +0100

    day 11, put the main part into a function

--- day11.py
+++ day11.py
@@ -9,51 +9,59 @@ import numpy as np
 from itertools import product
 
 
+def load_data(fname):
+    data = read_input(fname)
+    data_array = np.array([mapl(int, line) for line in data])
+    return data_array
+
+
 def get_neighbours(y, x):
     y1 = y-1 if (y-1 >= 0) else y
     y2 = y+2 if (y+1 < MAXY) else y+1
     x1 = x-1 if (x-1 >= 0) else x
     x2 = x+2 if (x+1 < MAXX) else x+1
-    yy, xx = np.arange(y1, y2), np.arange(x1, x2)
-    n_coord = set(product(yy, xx))
+    n_coord = set(product(np.arange(y1, y2), np.arange(x1, x2)))
     n_coord.remove((y,x))
     return n_coord
 
 
-# LOAD DATA
-# data = read_input('example11')
-data = read_input(11)
-data_array = np.array([mapl(int, line) for line in data])
-MAXY, MAXX = data_array.shape
+def increase_energy(should_flash, energy_level):
+    for octopus in zip(*np.where(should_flash)):
+        for neighbour in get_neighbours(*octopus):
+            if energy_level[neighbour] > 0: energy_level[neighbour] += 1
+    return energy_level
 
 
-# MAIN
-nr_flashes, step, synced = 0, 0, 0
-while not synced:
-    step += 1
-    flashed = np.zeros_like(data_array).astype(bool)
-    data_array += 1
-    should_flash = (data_array > 9) & (~flashed)
+def main(energy_level):
+    nr_flashes, step = 0, 0
+    while True:
 
-    while should_flash.any():
-        data_array[should_flash] = 0  # flash! ahaaaa!
-        flashed[should_flash] = True
-        nr_flashes += should_flash.sum()
+        step += 1
+        energy_level += 1
+        should_flash = (energy_level > 9)
 
-        # increase energy of neighbours
-        for octopus in zip(*np.where(should_flash)):
-            for ny, nx in  get_neighbours(*octopus):
-                if ~flashed[ny, nx]: data_array[ny, nx] += 1
+        while should_flash.any():
 
-        # identify neighbours that should flash
-        should_flash = (data_array > 9) & (~flashed)
+            energy_level[should_flash] = 0  # flash! ahaaaa!
+            nr_flashes += should_flash.sum()
 
-        # condition for part 2
-        synced = (True if (data_array == 0).all() else False)
+            # increase energy of neighbours
+            energy_level = increase_energy(should_flash, energy_level)
 
-    # part 1 (number of flashes after 100 steps)
-    if step == 100:
-        p1 = nr_flashes
+            # identify neighbours that should flash
+            should_flash = (energy_level > 9)
 
+        # part 1 (number of flashes after 100 steps)
+        if step == 100:
+            p1 = nr_flashes
+
+        # part 2 (sync)
+        if (energy_level == 0).all():
+            return p1, step
+
+
+energy_level = load_data(11)
+MAXY, MAXX = energy_level.shape
+p1, p2 = main(energy_level)
 print(p1)
-print(step)
+print(p2)
